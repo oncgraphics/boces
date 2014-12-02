c73bfc84b7b2c069cb6a5eed029eb01be2a7cbb4
alexwolcott <alexwolcott@users.noreply.github.com>
2014-12-02 12:27:48 -0500

    Update blank.js

--- js/blank.js
+++ js/blank.js
@@ -1,1 +1,11 @@
-*
+$(document).ready(function() {
+  $("#cssmenu").menumaker({
+    title: "Menu",
+    format: "dropdown"
+  });
+
+  $("#cssmenu a").each(function() {
+  	var linkTitle = $(this).text();
+  	$(this).attr('data-title', linkTitle);
+  });
+});
